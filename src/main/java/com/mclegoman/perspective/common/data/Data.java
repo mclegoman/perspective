df56a3ed7d88ceafe3b7d762fde11ea9e27d4e0a
dannytaylor <daniel@mclegoman.com>
2025-01-13 21:33:12 +1300

    Update Versioning

--- src/main/java/com/mclegoman/perspective/common/data/Data.java
+++ src/main/java/com/mclegoman/perspective/common/data/Data.java
@@ -7,9 +7,9 @@
 
 package com.mclegoman.perspective.common.data;
 
-import com.mclegoman.luminance.common.util.ReleaseType;
 import com.mclegoman.luminance.common.util.Version;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class Data extends com.mclegoman.luminance.common.data.Data {
-	public static final Version version = Version.create("Perspective", "perspective", 1, 3, 0, ReleaseType.ALPHA, 9, "6CTGnrNg");
+	public static final Version version = Version.parse(FabricLoader.getInstance().getModContainer("perspective").get().getMetadata(), "6CTGnrNg");
 }
