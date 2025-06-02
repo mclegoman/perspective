f8eca620d70813ebb025d6a503bf5ea8c365a705
dannytaylor <daniel@mclegoman.com>
2025-06-02 13:50:29 +1200

    adjusted default multiplier.

--- common/src/main/java/com/mclegoman/perspective/client/config/PerspectiveDefaultConfig.java
+++ common/src/main/java/com/mclegoman/perspective/client/config/PerspectiveDefaultConfig.java
@@ -53,10 +53,10 @@ public class PerspectiveDefaultConfig extends ReflectiveConfig {
 	@IntegerRange(min = 1, max = 10)
 	public final TrackedValue<Integer> holdPerspectiveMultiplierIncrementSize = this.value(5);
 	@SerializedName("hold_perspective_back_multiplier")
-	@FloatRange(min = 0.5, max = 8)
+	@FloatRange(min = 0.5, max = 16)
 	public final TrackedValue<Float> holdPerspectiveBackMultiplier = this.value(1.0F);
 	@SerializedName("hold_perspective_front_multiplier")
-	@FloatRange(min = 0.5, max = 8)
+	@FloatRange(min = 0.5, max = 16)
 	public final TrackedValue<Float> holdPerspectiveFrontMultiplier = this.value(1.0F);
 	@SerializedName("hold_perspective_back_hide_hud")
 	public final TrackedValue<Boolean> holdPerspectiveBackHideHud = this.value(false);
