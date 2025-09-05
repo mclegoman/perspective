3910263d20db50d73abbd79c382955fb1ca515da
dannytaylor <daniel@mclegoman.com>
2025-09-05 22:03:40 +1200

    added a string to id configentry that uses perspective's namespace

--- perspective-config/src/main/java/dev/dannytaylor/perspective/config/api/config/ConfigEntry.java
+++ perspective-config/src/main/java/dev/dannytaylor/perspective/config/api/config/ConfigEntry.java
@@ -7,6 +7,7 @@
 
 package dev.dannytaylor.perspective.config.api.config;
 
+import dev.dannytaylor.perspective.base.Perspective;
 import folk.sisby.kaleido.api.ReflectiveConfig;
 import net.minecraft.util.Identifier;
 
@@ -16,4 +17,10 @@ import net.minecraft.util.Identifier;
  * config: This is your config class, See Quilt Config's docs for more information, you do not need to make an instance as we'll make one for you.
  */
 public record ConfigEntry(Identifier id, Class<? extends ReflectiveConfig> config) {
+    /**
+     * Uses `Perspective.id` as the identifier namespace.
+     */
+    public ConfigEntry(String path, Class<? extends ReflectiveConfig> config) {
+        this(Perspective.ofId(path), config);
+    }
 }
